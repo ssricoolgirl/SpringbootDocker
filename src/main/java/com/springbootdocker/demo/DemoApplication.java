c2518067b89261bd10dfe665d9a70b9f5a99f3d9
ssricoolgirl <102844143+ssricoolgirl@users.noreply.github.com>
2023-05-03 19:05:55 -0500

    Initial commit

---
+++ src/main/java/com/springbootdocker/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.springbootdocker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@GetMapping("/message")
+	public String getMessage(){
+		return "Hi, Welcome to Docker";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
